fbaac0b6b37ca25ca5a11e0f3e5aaeb1e1b17007
German Gomez-Herrero <g@germangh.com>
2015-10-16 15:43:58 +0200

    More tests

--- tests/test_ec2.py
+++ tests/test_ec2.py
@@ -4,6 +4,7 @@
 import pytest
 import uuid
 from boto3facade.ec2 import Ec2
+import boto3
 
 
 @pytest.yield_fixture(scope="module")
@@ -14,6 +15,21 @@ def randomstr():
 @pytest.yield_fixture(scope="module")
 def ec2():
     yield Ec2()
+
+
+@pytest.yield_fixture(scope='module')
+def ec2client():
+    yield boto3.client('ec2')
+
+
+@pytest.yield_fixture(scope="module")
+def testvpc(randomstr, ec2client):
+    resp = ec2client.create_vpc(CidrBlock='10.49.0.0/16')
+    vpcid = resp['Vpc']['VpcId']
+    vpc = boto3.resource('ec2').Vpc(vpcid)
+    vpc.create_tags(Tags=[{'Key': 'Name', 'Value': randomstr}])
+    yield vpc
+    ec2client.delete_vpc(VpcId=vpcid)
 
 
 def test_get_ami_by_tag_no_match(ec2, randomstr):
@@ -35,3 +51,9 @@ def test_get_sg_by_name_no_match(ec2, randomstr):
 def test_get_vpc_by_name_no_math(ec2, randomstr):
     vpc = ec2.get_vpc_by_name(randomstr)
     assert list(vpc) == []
+
+
+def test_get_vpc_by_name(ec2, randomstr, testvpc):
+    vpc = list(ec2.get_vpc_by_name(randomstr))
+    assert len(vpc) == 1
+    assert vpc[0].id == testvpc.id
