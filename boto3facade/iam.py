c20e369cbf2f6c225d5d439b4a8bb7b727e95c33
German Gomez-Herrero <german@innovativetravel.eu>
2017-04-02 20:25:08 +0200

    Get IAM user property

--- boto3facade/iam.py
+++ boto3facade/iam.py
@@ -10,7 +10,7 @@ class Iam(AwsFacade):
         return 'iam'
 
     def get_instance_profile_by_id(self, profile_id):
-        """Produces an InstanceProfile object for the provided profile Id"""
+        """Produce an InstanceProfile object for the provided profile Id"""
         profiles = self.client.list_instance_profiles().get('InstanceProfiles')
         if profiles is None:
             return
@@ -18,3 +18,13 @@ class Iam(AwsFacade):
         if len(pinfo) < 1:
             return
         return self.resource.InstanceProfile(pinfo[0]['InstanceProfileName'])
+
+
+    def get_user_property(self, property_name, user_name=None):
+        """Get a property from an IAM user."""
+        if user_name:
+            user_meta = self.client.get_user(UserName=user_name)
+        else:
+            user_meta = self.client.get_user()
+
+        return user_meta['User'][property_name]
